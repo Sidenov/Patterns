f88e005e968a8193e422a2971a6e40104ed8c69d
Александр Сиденов <sid_san@mail.ru>
2022-06-16 10:32:03 +0800

    Add Allure

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -2,35 +2,51 @@ package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.conditions.ExactText;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
-import java.time.Duration;
-import java.util.Locale;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
-import com.github.javafaker.Faker;
 
 public class CardDeliveryTest {
+    private final DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("Ru");
+    private final int daysToAddFOrFirstMeeting = 5;
+    private final String firstMeetingDate = DataGenerator.generateDate((daysToAddFOrFirstMeeting));
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
 
     @Test
+    @DisplayName("Успешное планирование встречи")
     void shouldRegisterByAccount() {
-        String planningDate = DataGenerator.generateDate(5);
         Configuration.holdBrowserOpen = true;
-        open("http://localhost:9999");
 
-        $("[placeholder=\"Город\"]").setValue(DataGenerator.generateCity("ru"));
+        $("[placeholder=\"Город\"]").setValue(validUser.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[placeholder=\"Дата встречи\"]").setValue(planningDate);
-        $("[data-test-id=\"name\"] input").setValue(DataGenerator.generateName("ru"));
-        $("[data-test-id=\"phone\"] input").setValue(DataGenerator.generatePhone("ru"));
+        $("[placeholder=\"Дата встречи\"]").setValue(firstMeetingDate);
+        $("[data-test-id=\"name\"] input").setValue(validUser.getName());
+        $("[data-test-id=\"phone\"] input").setValue(validUser.getPhone());
         $("[data-test-id=\"agreement\"]").click();
         $x("//*[text()=\"Запланировать\"]").click();
         $(".notification__content")
-                .shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate));
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate));
 
         //Меняем дату
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
@@ -43,6 +59,23 @@ public class CardDeliveryTest {
         $(".notification__content")
                 .shouldHave(Condition.text("Встреча успешно запланирована на " + newDate));
 
+    }
+
+    @Test
+    @DisplayName("Поялвение ошибки при вводе неверного формата мобильного телефона")
+    void shouldGetErrorIfWrongPhone() {
+
+        Configuration.holdBrowserOpen = true;
+
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(firstMeetingDate);
+        $("[data-test-id='phone'] input").setValue(DataGenerator.generatePhone("en"));
+        $("[data-test-id='agreement']").click();
+        $(byText("Запланировать")).click();
+        $("[data-test-id='phone'] input_sub")
+                .shouldHave(new ExactText("Неверный формат номера мобильного телефона"));
     }
 
 }
--- src/test/java/ru/netology/web/DataGenerator.java
+++ src/test/java/ru/netology/web/DataGenerator.java
@@ -23,9 +23,20 @@ public class DataGenerator {
         Faker faker = new Faker(new Locale(locale));
         return faker.phoneNumber().phoneNumber();
     }
-    public static String generateCity(String locale) {
-        Faker faker = new Faker(new Locale(locale));
-        return faker.address().city();
+
+    public static String generateCity() {
+        var cities = new String[]{"Майкоп", "Горно-Алтайск", "Уфа", "Улан-Удэ",
+                "Махачкала", "Магас", "Нальчик", "Элиста", "Черкесск", "Петрозаводск", "Сыктывкар", "Симферополь",
+                "Йошкар-Ола", "Саранск", "Якутск", "Владикавказ", "Казань", "Кызыл", "Ижевск", "Абакан", "Грозный",
+                "Чебоксары", "Барнаул", "Чита", "Петропавловск-Камчатский", "Краснодар", "Красноярск", "Пермь",
+                "Владивосток", "Ставрополь", "Хабаровск", "Благовещенск", "Архангельск", "Астрахань", "Белгород",
+                "Брянск", "Владимир", "Волгоград", "Вологда", "Воронеж", "Иваново", "Иркутск", "Калининград", "Калуга",
+                "Кемерово", "Киров", "Кострома", "Курган", "Курск", "Гатчина", "Липецк", "Магадан", "Красногорск",
+                "Мурманск", "Нижний Новгород", "Великий Новгород", "Новосибирск", "Омск", "Оренбург", "Орёл", "Пенза",
+                "Псков", "Ростов-на-Дону", "Рязань", "Самара", "Саратов", "Южно-Сахалинск", "Екатеринбург", "Смоленск",
+                "Тамбов", "Тверь", "Томск", "Тула", "Тюмень", "Ульяновск", "Челябинск", "Ярославль", "Москва",
+                "Санкт-Петербург", "Севастополь", "Биробиджан", "Нарьян-Мар", "Ханты-Мансийск", "Анадырь", "Салехард"};
+        return cities[new Random().nextInt(cities.length)];
     }
 
     public static class Registration {
@@ -33,7 +44,7 @@ public class DataGenerator {
 
 
         public static UserInfo generateUser(String locale) {
-            return new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
         }
     }
 
